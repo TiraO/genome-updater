0a9e33b0d953264506653313a90cca87b8209be0
Tira Odhner <atiraodhner@gmail.com>
2022-01-19 21:48:31 -0500

    print more information during the run

--- example.js
+++ example.js
@@ -3,7 +3,9 @@ const {runInsertion} = require("./genome_inserter")
 
 return runInsertion("C://Users/Tira/bases.fasta",
   "C://Users/Tira/annotations.gtf",
-  "C://Tira/new_sequence.fasta",
-  "C://Users/Tira/new-annotation.gtf" // This gtf file should only have one line.
+  "C://Tira/sequence-to-insert.fasta",
+  "C://Users/Tira/annotation-to-insert.gtf", // This gtf file should only have one line. start/end in this file tells the code where the sequence should be inserted and how long it is.
+  // "C://Users/Tira/output-sequence.fasta",
+  // "C://Users/Tira/output-annotations.gtf",
 )
 
--- genome_inserter.js
+++ genome_inserter.js
@@ -2,14 +2,20 @@ const _ = require("underscore");
 const {loadFile, writeFile} = require("./file_helper");
 const {parseSequenceLine, formatSequenceLine, formatSequenceLines, parseSequenceText} = require("./sequence_format_utils");
 
-function offsetAnnotations(annotations, sequence) {
+function offsetAnnotations(annotations, sequence, stats = { offsetCount: 0}) {
   return annotations.map((annotation) => {
     let {start, end} = annotation;
+    let updated = false;
     if (start > sequence.annotation.start) {
       start += sequence.bases.length;
+      updated = true;
     }
     if (end > sequence.annotation.start) {
       end += sequence.bases.length;
+      updated = true;
+    }
+    if(updated){
+      stats.offsetCount ++;
     }
     return {
       ...annotation,
@@ -36,13 +42,18 @@ let insertSequence = (originalBases, originalGtfText, newSequence) => {
   if (newSequence.bases.length) {
     newSequence.annotation.end = newSequence.annotation.start + newSequence.bases.length
   }
+  console.log("inserting ", newSequence.bases.length, " new bases")
+  console.log("starting at ", newSequence.annotation.start, "ending at ", newSequence.annotation.end)
 
+
+  let stats = {offsetCount: 0};
   let allAnnotations = [
-    ...(offsetAnnotations(beforeAnnotations, newSequence)),
+    ...(offsetAnnotations(beforeAnnotations, newSequence, stats)),
     newSequence.annotation,
-    ...(offsetAnnotations(afterAnnotations, newSequence))
+    ...(offsetAnnotations(afterAnnotations, newSequence, stats))
   ];
 
+  console.log("updated ", stats.offsetCount, "annotations in the gtf file.");
   let gtfText = formatSequenceLines(allAnnotations);
   return {bases, annotations: allAnnotations, gtfText};
 }
@@ -67,7 +78,6 @@ const runInsertion = async (basesPath, gtfPath, basesToInsertPath, annotationToI
   let annotationToInsert = rowsWithCells[0];
 
   let newSequence = {bases: newBases, annotation: annotationToInsert};
-
   let {bases, gtfText} = insertSequence(originalBases, originalGtfText, newSequence)
 
   const updatedBasesPath = getOutputFilePath(outputBasesPath, basesPath);
